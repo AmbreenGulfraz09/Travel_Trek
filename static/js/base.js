cb016a2fbd85f5cbbdaaa8ca1d33a4cadb032193
Taimoor-47 <taimoorpervaiz47@gmail.com>
2024-10-21 12:50:43 +0500

    Updated

--- static/js/base.js
+++ static/js/base.js
@@ -1,10 +1,22 @@
-function setActive(link) {
-    // Get all nav links
-    const links = document.querySelectorAll('.navbar-nav .nav-link');
+// function setActive(link) {
+//     // Get all nav links
+//     const links = document.querySelectorAll('.nav-link.active');
 
-    // Remove active class from all links
-    links.forEach(l => l.classList.remove('active'));
+//     // Remove active class from all links
+//     links.forEach(l => l.classList.remove(''));
 
-    // Add active class to the clicked link
-    link.classList.add('active');
-}
+//     // Add active class to the clicked link
+//     link.classList.add('');
+// }
+// function setActive(link) {
+//     // Get all nav links
+//     const links = document.querySelectorAll('.nav-link.active');
+
+//     // Remove active class from all links
+//     links.forEach(l => l.classList.remove('active'));
+
+//     // Add active class to the clicked link
+//     link.classList.add('active');
+// }
+// In your static/js/main.js
+
